f9437745fc762e4f800605ea9cdc2a2ce821423b
Anand-Verma05 <anandvermatech05@gmail.com>
2026-07-20 20:13:30 +0530

    net not wokring

--- core/transcriber.py
+++ core/transcriber.py
@@ -4,7 +4,7 @@ import os
 WHISPER_MODEL=os.getenv("WHISPER_MODEL","small")
 
 
-_modle=None
+_model=None
 
 def load_model():
 
@@ -17,6 +17,23 @@ def load_model():
 
     return _model
 
-def transcribe_chunk(chunk_path):
-        
+def transcribe_chunk(chunk_path:str,translate :bool=False)->str:
+    model=load_model()
+    task="translate" if translate else "transcribe"
+    result=model.transcribe(chunk_path,task=task)
+
+    return result['text']
+
+def transcribe_all(chunks:list,translate : bool=False)->str:
+    full_transcript=""
+
+    for i,chunk in enumerate(chunks):
+        print(f"transcribing {i+1}th chunk")
+        text=transcribe_chunk(chunk,translate=translate)
+
+        full_transcript=text + " "
+    
+    return full_transcript
+    
+    print("Transcription done")
 
